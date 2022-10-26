285c2f113969c0c59eaab7c6965b1785d902c1d8
YakhyojonKholmatov <116672685+YakhyojonKholmatov@users.noreply.github.com>
2022-10-26 13:05:43 +0500

    Update CopiedNames.java

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -4,11 +4,11 @@ public class CopiedNames {
     public static void main(String[] args) {
         int num1 = 0;
         int num2 = 2000;
-        int idForMax = 1000;
-        int minimumValueOfR = 4000;
+        int id = 1000;
+        int minimum = 4000;
         int changed = 3000;
-        int fast1 = 5000;
-        int middleOfTheVariables = 7000;
-        int incomeOfFirstP = 90000;
+        int fast = 5000;
+        int middle = 7000;
+        int income = 90000;
     }
 }
